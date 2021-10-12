437a796650df724991a890a5e6cfa60cfbced2ed
Idowu Bamimore <idowubamimore@gmail.com>
2021-10-12 11:32:08 +0100

    angular components part II

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCard, MatCardModule } from '@angular/material/card';
+import { MatButton, MatButtonModule } from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -22,7 +25,10 @@ import { MenuComponent } from './menu/menu.component';
     BrowserAnimationsModule,
     FlexLayoutModule,
     MatToolbarModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
 
+
+const DISHES: Dish[] = [
+  {
+    id: '1',
+    name: 'Software Development',
+    image: '/assets/images/dice 1.png',
+    category: 'mains',
+    featured: true,
+    label: 'Hot',
+    price: '77.9',
+    //tslint:disable-next-line:max-line-length
+    description: 'A unique combination'
+  },
+    
+  {
+    id: '2',
+    name: 'Web Development',
+    image: '/assets/images/dice 2.png',
+    category: 'mains',
+    featured: true,
+    label: 'Hot',
+    price: '78.9',
+    //tslint:disable-next-line:max-line-length
+    description: 'A unique combination'
+  },
+
+  {
+    id: '3',
+    name: 'Data Science',
+    image: '/assets/images/dice 2.png',
+    category: 'mains',
+    featured: true,
+    label: 'Hot',
+    price: '78.9',
+    //tslint:disable-next-line:max-line-length
+    description: 'A unique combination'
+  },
+
+  {
+    id: '4',
+    name: 'Artificial Intelligence',
+    image: '/assets/images/dice 2.png',
+    category: 'mains',
+    featured: true,
+    label: 'Hot',
+    price: '78.9',
+    //tslint:disable-next-line:max-line-length
+    description: 'A unique combination'
+  }
+]
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -8,31 +58,9 @@ import { Dish } from '../shared/dish';
 })
 export class MenuComponent implements OnInit {
 
-  dishes: Dish[] = [
-    {
-      id: '0',
-      name: 'Uthappizza',
-      image: '/assets/images/dice 1.png',
-      category: 'mains',
-      featured: true,
-      label: 'Hot',
-      price: '77.9',
-      //tslint:disable-next-line:max-line-length
-      description: 'A unique combination'
-    },
-      
-    {
-      id: '1',
-      name: 'Uthappizza',
-      image: '/assets/images/dice 2.png',
-      category: 'mains',
-      featured: true,
-      label: 'Hot',
-      price: '78.9',
-      //tslint:disable-next-line:max-line-length
-      description: 'A unique combination'
-    }
-  ]
+  dishes: Dish[] = DISHES;
+
+  selectedDish: Dish = DISHES[0];
 
   constructor() { }
 
